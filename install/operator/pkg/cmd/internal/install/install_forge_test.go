eddbc3f6437fafe6d61fa80b6db3ad30496022eb
Luis Garcia Acosta <lgarciaac@gmail.com>
2019-11-12 17:46:59 +0100

    bring install forge back

---
+++ install/operator/pkg/cmd/internal/install/install_forge_test.go
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package install
+
+import "testing"
+
+func Test_convertToParam(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertToParam(tt.args.name); got != tt.want {
+				t.Errorf("convertToParam() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
