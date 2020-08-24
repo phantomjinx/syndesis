35380b2fec1b5b9f9066843e8b40ef78ec4bf3a2
Luis Garcia Acosta <lgarciaac@gmail.com>
2020-08-24 10:29:44 +0200

    When we build in fuse-online we dont generate the assets, therefore this file needs to contains the final db productized image. Source: https://github.com/jboss-fuse/fuse-online-operator/blob/private-devel-fuse-7.7-openshift-rhel-7/Dockerfile#L14
    Fixes https://issues.redhat.com/browse/ENTESB-14436?filter=12341128
    
    Change CSV output to camel
    
    chore: change package name (remove -operator)
    chore: change channel name adding `.x`
    chore: fix tests
    
    chore: fix tests
    
    chore: fix psql version
    
    chore: pin psql image version
    
    Fix postgresql image and amq-example template
    
    config.yaml: The postgresql image should be the same as in pkg/cmd/internal/install/install_defaults.go
    04-amq-example.yml.tmpl: add missing "end"

--- install/operator/pkg/syndesis/olm/csv.go
+++ install/operator/pkg/syndesis/olm/csv.go
@@ -106,7 +106,7 @@ type Labels struct {
 }
 
 type Selector struct {
-	MatchLabels Label
+	MatchLabels Label `json:"matchLabels"`
 }
 
 type Link struct {
@@ -138,13 +138,13 @@ type Install struct {
 }
 
 type InstallSpec struct {
-	ClusterPermissions []InstallSpecPermission
+	ClusterPermissions []InstallSpecPermission `json:"clusterPermissions"`
 	Permissions        []InstallSpecPermission
 	Deployments        []InstallSpecDeployment
 }
 
 type InstallSpecPermission struct {
-	ServiceAccountName string
+	ServiceAccountName string `json:"serviceAccountName"`
 	Rules              interface{}
 }
 
@@ -161,14 +161,14 @@ type CustomResourceDefinition struct {
 	Name        string
 	Version     string
 	Kind        string
-	DisplayName string
+	DisplayName string `json:"displayName"`
 	Description string
 }
 
 // In order to build the body for both upstream and downstream,
 // set variables accordingly
 func (c *csv) setVariables() {
-	c.version = c.config.Version
+	c.version = c.config.Version + ".x"
 	c.maturity = "alpha"
 
 	// Both of these are required to ensure permissions are correctly added to manifest
@@ -269,6 +269,8 @@ func (c *csv) build() (err error) {
 		ApiVersion: "operators.coreos.com/v1alpha1",
 		Kind:       "ClusterServiceVersion",
 		Metadata: Metadata{
+			//TODO: This has to be manually replace because `opm` expects a semver format version as per
+			// https://semver.org/
 			Name:      c.name + ".v" + c.version,
 			Namespace: "placeholder",
 			Annotations: MetadataAnnotations{
