314d5aade4a7dc74bb9c3f6895608ec95e6e41de
Claudio Miranda <claudio@claudius.com.br>
2020-09-11 15:10:39 -0300

    feat: Adopt the new label format for Fuse Online
    https://issues.redhat.com/browse/ENTESB-14398

--- install/operator/pkg/cmd/internal/install/install_defaults.go
+++ install/operator/pkg/cmd/internal/install/install_defaults.go
@@ -2,4 +2,4 @@ package install
 
 const defaultDatabaseImage = "registry.redhat.io/rhscl/postgresql-96-rhel7:latest"
 const productizedBuild = true
-const productVersion = "7.7"
+const productVersion = "7.8"
