f4863cae20361372db6dcbef96a01a1a3ec97b01
Luis Garcia Acosta <lgarciaac@gmail.com>
2020-09-29 13:01:27 +0200

    chore: change package name (remove -operator) chore: change channel name adding `.x` chore: fix tests

--- install/operator/pkg/syndesis/olm/csv.go
+++ install/operator/pkg/syndesis/olm/csv.go
@@ -168,7 +168,7 @@ type CustomResourceDefinition struct {
 // In order to build the body for both upstream and downstream,
 // set variables accordingly
 func (c *csv) setVariables() {
-	c.version = c.config.Version
+	c.version = c.config.Version + ".x"
 	c.maturity = "alpha"
 
 	// Both of these are required to ensure permissions are correctly added to manifest
@@ -269,6 +269,8 @@ func (c *csv) build() (err error) {
 		ApiVersion: "operators.coreos.com/v1alpha1",
 		Kind:       "ClusterServiceVersion",
 		Metadata: Metadata{
+			//TODO: This has to be manually replace because `opm` expects a semver format version as per
+			// https://semver.org/
 			Name:      c.name + ".v" + c.version,
 			Namespace: "placeholder",
 			Annotations: MetadataAnnotations{
