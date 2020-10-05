0eea6fe4bc9700d1da2f408643653c5b5d92050c
Luis Garcia Acosta <lgarciaac@gmail.com>
2020-10-05 15:23:31 +0200

    chore: fix psql version

--- install/operator/pkg/cmd/internal/install/install_defaults.go
+++ install/operator/pkg/cmd/internal/install/install_defaults.go
@@ -1,5 +1,5 @@
 package install
 
-const defaultDatabaseImage = "registry.redhat.io/rhscl/postgresql-96-rhel7:latest"
+const defaultDatabaseImage = "registry.redhat.io/rhscl/postgresql-10-rhel7:latest"
 const productizedBuild = true
 const productVersion = "7.8"
